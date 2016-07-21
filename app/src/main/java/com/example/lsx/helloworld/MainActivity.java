51668b0a520405f3e2b42152b71a1563d8e4cee5
jy <jy@163.com>
2016-07-21 16:20:52 +0800

    button可用

--- app/src/main/java/com/example/lsx/helloworld/MainActivity.java
+++ app/src/main/java/com/example/lsx/helloworld/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+     Button button= (Button) findViewById(R.id.content_main_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("lsx","Please input other!");
             }
         });
     }
